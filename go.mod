e8f64c8f18218d9cfc6a5dafcef6694fc5499d6f
Hikari Yoneyama <e205743@ie.u-ryukyu.ac.jp>
2021-12-10 10:47:30 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Yado-Kariwo/OS-2021
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+    import (
+        "fmt"
+        "github.com/Yado-Kariwo/OS-2021"
+    )
+    func main() {
+            fmt.Println("Hello, World!")
+            fmt.Println( fileWrite.Hello("e205743"))
+    }
